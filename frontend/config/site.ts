491dd7d9e34c3a4171a00e1fea052a309837655b
Enzo Hugonnier <enzohugonn@gmail.com>
2025-01-13 15:01:14 +0100

    chore(Project): Remove unused files, update project name

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,20 +1,12 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "blog-cc",
+  description: "The best blogging site that has ever existed :D",
   navItems: [
     {
       label: "Home",
       href: "/",
-    },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
     },
     {
       label: "Blog",
@@ -60,7 +52,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/nextui-org/nextui",
+    github: "https://github.com/Darleanow/BlogCC",
     twitter: "https://twitter.com/getnextui",
     docs: "https://nextui.org",
     discord: "https://discord.gg/9b6yyZKmH4",
