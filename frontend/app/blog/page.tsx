c14e1089c61ec406c2cf51866b1fd5d2673acbf8
Enzo Hugonnier <enzohugonn@gmail.com>
2025-01-14 09:30:10 +0100

    refactor(BlogPage): Make new structure match with the existing blog page

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -1,20 +1,48 @@
 "use client";
 
 import { Tabs, Tab } from "@nextui-org/tabs";
+import { useMemo, useState } from "react";
 
+import { useArticles } from "@/hooks/useArticles";
 import ArticlesList from "@/components/articles";
+import { Category } from "@/lib/models/category";
 
 export default function BlogPage() {
+  const { articles } = useArticles();
+  const [selectedCategory, setSelectedCategory] = useState<
+    string | number | null
+  >("all");
+
+  const categories = useMemo(() => {
+    if (!articles) return [];
+    const categoryMap = new Map<string, Category>();
+
+    articles.forEach((article) => {
+      article.categories.forEach((category) => {
+        if (!categoryMap.has(category.slug)) {
+          categoryMap.set(category.slug, category);
+        }
+      });
+    });
+
+    return Array.from(categoryMap.values());
+  }, [articles]);
+
   return (
     <section className="flex flex-col gap-4">
       <div className="flex flex-wrap gap-4">
-        <Tabs variant={"underlined"}>
-          <Tab key="it" title="IT" />
-          <Tab key="data-science" title="Data Science" />
-          <Tab key="cpp" title="C++" />
+        <Tabs
+          selectedKey={selectedCategory}
+          variant="underlined"
+          onSelectionChange={setSelectedCategory}
+        >
+          <Tab key="all" title="All" />
+          {categories.map((category) => (
+            <Tab key={category.slug} title={category.name} />
+          ))}
         </Tabs>
       </div>
-      <ArticlesList />
+      <ArticlesList selectedCategory={selectedCategory?.toString() ?? "all"} />
     </section>
   );
 }
