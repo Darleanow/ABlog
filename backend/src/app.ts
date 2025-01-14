b5cb8fecf5581df84fb34626ad8a9b51e5426a8b
Nithard Damien <damien.nithard@gmail.com>
2025-01-14 12:19:46 +0100

    feat(Auth): Add signup, signin, signout routes and controllers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,9 @@
-import { errorMiddleware } from './middleware/error.middleware';
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import articlesRoutes from './routes/articles.routes';
+import { errorMiddleware } from "./middleware/error.middleware";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import articlesRoutes from "./routes/articles.routes";
+import authRoutes from "./routes/auth.routes";
 import commentsRoutes from "./routes/comments.routes";
 import categoriesRoutes from "./routes/categories.routes";
 import tagsRoutes from "./routes/tags.routes";
@@ -16,10 +17,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/articles', articlesRoutes);
+app.use("/api/articles", articlesRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/comments', commentsRoutes);
 app.use('/api/tags', tagsRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use(errorMiddleware);
 
