964409a79ac0bd8f9b51c267417086a95ab7fd7b
Enzo Hugonnier <enzohugonn@gmail.com>
2025-01-14 09:13:01 +0100

    feat(API): Implement Article API

---
+++ frontend/lib/api/articles-api.ts
@@ -0,0 +1,16 @@
+import { Article } from "../models/article";
+import { API_CONFIG } from "../config/api.config";
+
+import { BaseApi } from "./base-api";
+
+export class ArticlesApi extends BaseApi {
+  async getArticles(): Promise<Article[]> {
+    return this.fetchApi<Article[]>(API_CONFIG.endpoints.articles);
+  }
+
+  async getArticlesByCategory(categorySlug: string): Promise<Article[]> {
+    return this.fetchApi<Article[]>(
+      `${API_CONFIG.endpoints.articles}?category=${categorySlug}`,
+    );
+  }
+}
