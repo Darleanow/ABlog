8e6be18e83900bc81b09e7024c6c37d533af02a0
Enzo Hugonnier <enzohugonn@gmail.com>
2025-01-16 01:17:14 +0100

    refactor(index): Use coelescing operator for conditions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { supabase } from "./config/database";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const testConnection = async () => {
   try {
